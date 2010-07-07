e75de14df4f0d1170a50bc2d9b2c90ea9e3f611e
okram <okrammarko@gmail.com>
2010-07-07 17:19:42 -0600

    added future filter pipe to Pipes as its probably generally useful beyond Gremlin.

---
+++ src/main/java/com/tinkerpop/pipes/filter/FutureFilterPipe.java
@@ -0,0 +1,30 @@
+package com.tinkerpop.pipes.filter;
+
+import com.tinkerpop.pipes.AbstractPipe;
+import com.tinkerpop.pipes.Pipe;
+import com.tinkerpop.pipes.SingleIterator;
+
+import java.util.NoSuchElementException;
+
+/**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class FutureFilterPipe<S> extends AbstractPipe<S, S> implements FilterPipe<S> {
+
+    private final Pipe<S, ?> pipe;
+
+    public FutureFilterPipe(final Pipe<S, ?> pipe) {
+        this.pipe = pipe;
+    }
+
+    public S processNextStart() {
+        while (this.starts.hasNext()) {
+            S s = this.starts.next();
+            pipe.setStarts(new SingleIterator<S>(s));
+            if (pipe.hasNext()) {
+                return s;
+            }
+        }
+        throw new NoSuchElementException();
+    }
+}
---
+++ src/test/java/com/tinkerpop/pipes/filter/FutureFilterPipeTest.java
@@ -0,0 +1,38 @@
+package com.tinkerpop.pipes.filter;
+
+import com.tinkerpop.pipes.BaseTest;
+import com.tinkerpop.pipes.IdentityPipe;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class FutureFilterPipeTest extends BaseTest {
+
+    public void testBasicFutureFilter() {
+        List<String> names = Arrays.asList("marko", "povel", "peter", "josh");
+        FutureFilterPipe<String> pipe1 = new FutureFilterPipe<String>(new IdentityPipe<String>());
+        pipe1.setStarts(names);
+        int counter = 0;
+        while (pipe1.hasNext()) {
+            counter++;
+            pipe1.next();
+        }
+        assertEquals(counter, 4);
+    }
+
+    public void testAdvancedFutureFilter() {
+        List<String> names = Arrays.asList("marko", "povel", "peter", "josh");
+        FutureFilterPipe<String> pipe1 = new FutureFilterPipe<String>(new CollectionFilterPipe<String>(Arrays.asList("marko", "povel"), ComparisonFilterPipe.Filter.EQUAL));
+        pipe1.setStarts(names);
+        int counter = 0;
+        while (pipe1.hasNext()) {
+            counter++;
+            String name = pipe1.next();
+            assertTrue(name.equals("peter") || name.equals("josh"));
+        }
+        assertEquals(counter, 2);
+    }
+}
