44c278b5e07931d3947c1f017f031fe53716a06e
okram <okrammarko@gmail.com>
2010-10-13 09:57:06 -0600

    moved ScatterPipe from Gremlin to Pipes as its a generally useful Pipe.

---
+++ src/main/java/com/tinkerpop/pipes/util/ScatterPipe.java
@@ -0,0 +1,32 @@
+package com.tinkerpop.pipes.util;
+
+import com.tinkerpop.pipes.AbstractPipe;
+
+import java.util.Iterator;
+
+/**
+ * ScatterPipe will unroll any iterator/iterable that is inputted into it.
+ * This will only occur for one level deep. It will not unroll an iterator within an iterator, etc.
+ *
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class ScatterPipe<S, E> extends AbstractPipe<S, E> {
+
+    private Iterator<E> tempIterator;
+
+    public E processNextStart() {
+        while (true) {
+            if (null != this.tempIterator && this.tempIterator.hasNext()) {
+                return this.tempIterator.next();
+            } else {
+                Object object = this.starts.next();
+                if (object instanceof Iterator)
+                    this.tempIterator = (Iterator<E>) object;
+                else if (object instanceof Iterable)
+                    this.tempIterator = ((Iterable<E>) object).iterator();
+                else
+                    return (E) object;
+            }
+        }
+    }
+}
