923571891a126630c2d3e6baf9081fc86cb11225
okram <okrammarko@gmail.com>
2011-01-09 22:45:04 -0600

    AggregatorPipe now behaves like a true sideeffect pipe. To get the previous effect you now have to do new SideEffectPipe(new AggregatorPipe()) --> new ScatterPipe().

--- src/main/java/com/tinkerpop/pipes/sideeffect/AggregatorPipe.java
+++ src/main/java/com/tinkerpop/pipes/sideeffect/AggregatorPipe.java
@@ -4,34 +4,31 @@ package com.tinkerpop.pipes.sideeffect;
 import com.tinkerpop.pipes.AbstractPipe;
 
 import java.util.Collection;
-import java.util.Iterator;
+import java.util.LinkedList;
 
 /**
  * The AggregatorPipe produces a side effect that is the provided Collection filled with the contents of all the objects that have passed through it.
- * Before the first object is emitted from the AggregatorPipe, all of its incoming objects have been aggregated into the collection.
- * The collection iterator is used as the emitting iterator. Thus, what goes into AggregatorPipe may not be the same as what comes out of AggregatorPipe.
- * For example, duplicates removed, different order to the stream, etc.
- * Finally, note that different Collections have different behaviors and write/read times.
+ * The incoming objects are emitted as is.
+ * Note that different Collections have different behaviors and write/read times.
  *
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
 public class AggregatorPipe<S> extends AbstractPipe<S, S> implements SideEffectPipe<S, S, Collection<S>> {
 
     private final Collection<S> aggregate;
-    private Iterator<S> aggregateIterator = null;
+
+    public AggregatorPipe() {
+        this.aggregate = new LinkedList<S>();
+    }
 
     public AggregatorPipe(final Collection<S> collection) {
         this.aggregate = collection;
     }
 
     protected S processNextStart() {
-        if (null == this.aggregateIterator) {
-            while (this.starts.hasNext()) {
-                aggregate.add(this.starts.next());
-            }
-            aggregateIterator = aggregate.iterator();
-        }
-        return this.aggregateIterator.next();
+        final S start = this.starts.next();
+        this.aggregate.add(start);
+        return start;
     }
 
     public Collection<S> getSideEffect() {
