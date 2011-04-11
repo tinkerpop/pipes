c59fdea2e61b5b91f8cac53434c089f461fa6d69
Marko Rodriguez <okrammarko@gmail.com>
2011-04-11 09:31:58 -0600

    added BackFilter. FutureFilters behavior was modified.

---
+++ src/main/java/com/tinkerpop/pipes/filter/BackFilterPipe.java
@@ -0,0 +1,47 @@
+package com.tinkerpop.pipes.filter;
+
+import com.tinkerpop.pipes.AbstractPipe;
+import com.tinkerpop.pipes.MetaPipe;
+import com.tinkerpop.pipes.Pipe;
+import com.tinkerpop.pipes.SingleIterator;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * BackFilterPipe will fully process the object through its internal pipe.
+ * If the internal pipe yields results, then the original object is emitted from the BackFilterPipe.
+ *
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class BackFilterPipe<S> extends AbstractPipe<S, S> implements FilterPipe<S>, MetaPipe {
+
+    private final Pipe<S, ?> pipe;
+
+    public BackFilterPipe(final Pipe<S, ?> pipe) {
+        this.pipe = pipe;
+    }
+
+    public S processNextStart() {
+        while (true) {
+            S s = this.starts.next();
+            this.pipe.reset();
+            this.pipe.setStarts(new SingleIterator<S>(s));
+            if (pipe.hasNext()) {
+                while (pipe.hasNext()) {
+                    pipe.next();
+                }
+                return s;
+            }
+        }
+    }
+
+    public String toString() {
+        return super.toString() + "<" + this.pipe + ">";
+    }
+
+    public List<Pipe> getPipes() {
+        return (List) Arrays.asList(this.pipe);
+    }
+
+}
