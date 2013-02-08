a86e8fc1764230eccfd433e405b7d23ae78b1dc9
Marko A. Rodriguez <okrammarko@gmail.com>
2013-02-08 14:06:43 -0700

    added a couple more OrderMapPipe test cases.

--- src/test/java/com/tinkerpop/pipes/transform/OrderMapPipeTest.java
+++ src/test/java/com/tinkerpop/pipes/transform/OrderMapPipeTest.java
@@ -1,7 +1,9 @@
 package com.tinkerpop.pipes.transform;
 
 import com.tinkerpop.pipes.Pipe;
+import com.tinkerpop.pipes.PipeFunction;
 import com.tinkerpop.pipes.util.PipeHelper;
+import com.tinkerpop.pipes.util.structures.Pair;
 import junit.framework.TestCase;
 
 import java.util.Arrays;
@@ -41,6 +43,46 @@ public class OrderMapPipeTest extends TestCase {
         assertEquals(list.get(2), "c");
         assertEquals(list.get(1), "b");
         assertEquals(list.get(0), "a");
+        assertEquals(list.size(), 3);
+    }
+
+    public void testPipeBasicCompareFunction() {
+        Pipe pipe = new OrderMapPipe<String>(new PipeFunction<Pair<Map.Entry<String, ?>, Map.Entry<String, ?>>, Integer>() {
+            @Override
+            public Integer compute(Pair<Map.Entry<String, ?>, Map.Entry<String, ?>> argument) {
+                return ((Comparable)argument.getB().getValue()).compareTo(argument.getA().getValue());
+            }
+        });
+        Map map = new HashMap();
+        map.put("c", 3);
+        map.put("a", 10);
+        map.put("b", 5);
+
+        pipe.setStarts(Arrays.asList(map));
+        List<String> list = PipeHelper.makeList(pipe);
+        assertEquals(list.get(2), "c");
+        assertEquals(list.get(1), "b");
+        assertEquals(list.get(0), "a");
+        assertEquals(list.size(), 3);
+    }
+
+    public void testPipeBasicCompareFunction2() {
+        Pipe pipe = new OrderMapPipe<String>(new PipeFunction<Pair<Map.Entry<String, ?>, Map.Entry<String, ?>>, Integer>() {
+            @Override
+            public Integer compute(Pair<Map.Entry<String, ?>, Map.Entry<String, ?>> argument) {
+                return ((Comparable)argument.getB().getValue()).compareTo(argument.getA().getValue());
+            }
+        });
+        Map map = new HashMap();
+        map.put("c", "c");
+        map.put("a", "a");
+        map.put("b", "b");
+
+        pipe.setStarts(Arrays.asList(map));
+        List<String> list = PipeHelper.makeList(pipe);
+        assertEquals(list.get(2), "a");
+        assertEquals(list.get(1), "b");
+        assertEquals(list.get(0), "c");
         assertEquals(list.size(), 3);
     }
 }
