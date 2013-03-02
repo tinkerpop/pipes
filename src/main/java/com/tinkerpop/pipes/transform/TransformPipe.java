dd49c79e29df87b640fe4a1d1a04b6504f83669e
Marko A. Rodriguez <okrammarko@gmail.com>
2013-03-02 11:09:24 -0700

    JavaDoc for TransformPipe.

--- src/main/java/com/tinkerpop/pipes/transform/TransformPipe.java
+++ src/main/java/com/tinkerpop/pipes/transform/TransformPipe.java
@@ -3,6 +3,8 @@ package com.tinkerpop.pipes.transform;
 import com.tinkerpop.pipes.Pipe;
 
 /**
+ * A TransformPipe will take an object of one type and transform it to an object of another type.
+ *
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
 public interface TransformPipe<S, E> extends Pipe<S, E> {
