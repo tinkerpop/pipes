65d40d7743612611ca54d03b291336761fa1eaf3
Stephen Mallette <spmva@genoprime.com>
2013-03-17 11:16:27 -0400

    Add failing test for reduce closure as part of a loop.

--- src/test/java/com/tinkerpop/pipes/sideeffect/GroupByReducePipeTest.java
+++ src/test/java/com/tinkerpop/pipes/sideeffect/GroupByReducePipeTest.java
@@ -1,6 +1,8 @@
 package com.tinkerpop.pipes.sideeffect;
 
+import com.tinkerpop.pipes.Pipe;
 import com.tinkerpop.pipes.PipeFunction;
+import com.tinkerpop.pipes.branch.LoopPipe;
 import junit.framework.TestCase;
 
 import java.util.Arrays;
@@ -42,10 +44,49 @@ public class GroupByReducePipeTest extends TestCase {
         }
         assertEquals(counter, starts.size());
         Map<String, Integer> map = pipe.getSideEffect();
-        assertEquals(map.size(), 3);
-        assertEquals(map.get("m").intValue(), 5);
-        assertEquals(map.get("j").intValue(), 9);
-        assertEquals(map.get("p").intValue(), 10);
+        assertEquals(3, map.size());
+        assertEquals(5, map.get("m").intValue());
+        assertEquals(9, map.get("j").intValue());
+        assertEquals(10, map.get("p").intValue());
 
+    }
+
+
+    public void untestPipeWithLooping(){
+        GroupByReducePipe<String, String, Integer, Integer> groupByReducePipe = new GroupByReducePipe<String, String, Integer, Integer>(new PipeFunction<String, String>() {
+            public String compute(String argument) {
+                return argument.substring(0, 1);
+            }
+        }, new PipeFunction<String, Integer>() {
+            public Integer compute(String argument) {
+                return argument.length();
+            }
+        }, new PipeFunction<Iterator<Integer>, Integer>() {
+            public Integer compute(Iterator<Integer> args) {
+                int sum = 0;
+                while (args.hasNext()) {
+                    sum = sum + args.next();
+                }
+                return sum;
+            }
+        }
+        );
+        Pipe<String, String> pipe = new LoopPipe(groupByReducePipe, LoopPipe.createLoopsFunction(4));
+
+        List<String> starts = Arrays.asList("marko", "josh", "peter", "pavel", "james");
+        pipe.setStarts(starts);
+        int counter = 0;
+        while (pipe.hasNext()) {
+            counter++;
+            String string = pipe.next();
+            assertTrue(starts.contains(string));
+        }
+
+        assertEquals(counter, starts.size());
+        Map<String, Integer> map = groupByReducePipe.getSideEffect();
+        assertEquals(3, map.size());
+        assertEquals(15, map.get("m").intValue());
+        assertEquals(30, map.get("p").intValue());
+        assertEquals(27, map.get("j").intValue());
     }
 }
